56d4d83f327ffa3922630f33fa932da099c990bd
Daryna Hnidash <melissa998877@gmail.com>
2024-03-14 20:49:19 +0200

    images issue

--- src/helpers/sample.ts
+++ src/helpers/sample.ts
@@ -10,6 +10,6 @@ export const product: Product = {
   description: "This enchanting bouquet is ideal for any occasion, featuring delicate roses, vibrant tulips, and fragrant lilies. Carefully selected and artfully arranged, it expresses tenderness and love, making it a perfect gesture of affection.",
   price: 98,
   discount: 20,
-  img: '/img/flower.jpg',
+  img: 'img/flower.jpg',
   type: "mono"
 };
